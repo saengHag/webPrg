2bc652ce49f610905e8125ce3e2b39a031c9431b
Hanbyeol Lee <a36688796@gmail.com>
2024-04-12 17:49:12 +0900

    Fastapi test

--- app/main.py
+++ app/main.py
@@ -4,24 +4,36 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
-
+forTest = None
 
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
 
+Item(forTest)
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    forTest.is_offer = q
+    forTest.price = item_id
+def Empty():
+    if forTest == None:
+        Write()
+def Delete():
+    forTest = None
 
+@app.post("/items/{item_id}")
+def Write(item_id: int, q: Union[str, None] = None):
+    forTest.is_offer = q
+    forTest.price = item_id
 
 @app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+def Update(item_id: int, q: Union[str, None] = None):
+    forTest.is_offer = q
+    forTest.price = item_id
