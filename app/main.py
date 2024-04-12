bde0c8f1de4d762db67fc79de9e6fb649a1ba478
Hanbyeol Lee <a36688796@gmail.com>
2024-04-12 16:34:46 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,6 +10,6 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+@app.get("/items/{item_id}")    #{item_id}에 숫자를 입력 받을 수 있음. 변수와 같음
+def read_item(item_id: int, q: Union[str, None] = None):    #q에 아무것도 입력하지 않으면 Null값이 들어감
     return {"item_id": item_id, "q": q}
