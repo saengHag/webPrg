d8ff875feee41b4ab352ed520910c000aa07ed73
Hanbyeol Lee <a36688796@gmail.com>
2024-04-19 15:49:15 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,10 @@ app = FastAPI()
 @app.get("/")
 def root():
     URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCD=11&cityCd=12&apiKey=s0VBUy1c7ma5Gcp6E3Pyg0i0Kj7KMkmhUv7KORAy&returnType=json"
-    return {"Hello": "World"}
+    
+    contents = requests.get(URL).text
+
+    return { "message": contents }
 
 @app.get("/home")
 def home():
